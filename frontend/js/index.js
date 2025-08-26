5122024b3ad65b10118ea844699e6cfcc04a1a82
JoseMonsalve07 <00jsmonsalve@gmail.com>
2025-08-26 14:07:05 -0500

    Merge feature/aboutus into feature/adelantos (forced histories)

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,13 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-<<<<<<< HEAD
-=======
     if (window.location.pathname.endsWith("index.html")) {
         if (!localStorage.getItem("isLoggedIn")) {
             localStorage.clear();
         }
     }
 
->>>>>>> feature/aboutus
     const loginBtn = document.getElementById("login-btn");
     const registerBtn = document.getElementById("register-btn"); 
 
@@ -15,25 +12,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (loginBtn) {
         loginBtn.addEventListener("click", () => {
-<<<<<<< HEAD
         window.location.href = "./login.html";
       });
-=======
-            window.location.href = "./login.html";
-        });
->>>>>>> feature/aboutus
     }
 
     if (registerBtn) {
         registerBtn.addEventListener("click", () => {
-<<<<<<< HEAD
         window.location.href = "./register.html";
       });
     }
 });
-=======
-            window.location.href = "./register.html";
-        });
-    }
-});
->>>>>>> feature/aboutus
